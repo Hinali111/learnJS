f5cadce4df7276f874618722d28e5dc8a95b4d5c
hinali111 <hinali7766@gmail.com>
2025-04-16 17:44:53 +0530

    sort

--- 1Basics/array.js
+++ 1Basics/array.js
@@ -30,18 +30,21 @@ const employers=[
         name:"Janvi",
         id:"103",
         age:25,
+        gpa:3.5,
         employees:["Kiara","Roshni","Krisha"]
     },
     {
         name:"Raj",
         id:"101",
         age:23,
+        gpa:1.5,
         employees:["Savi","Meera"]
     },
     {
         name:"Raj",
         id:"102",
         age:29,
+        gpa:2.5,
         employees:["sakhi"]
     }
 ]
@@ -70,4 +73,53 @@ const lastIdx= employers.findLastIndex((employer)=>employer["name"]==="Raj");
 const lastof25= employers.findLast((employer)=>employer["age"]>=25);
 const up25= employers.filter((employer)=>employer["age"]>=25);
 // const up25= employers.map((employer)=>employer["age"]>=25);
-console.log(findId);
+
+//! Array Sort
+const people = ["Rani","Aniket","Savi","Meera","Darshan","Roshni","Misha"];
+people.sort();
+// numbers  sorting
+const nums=[1,2,3,4,5,6,7,8,9,10,0,13,14];
+nums.sort(function(a,b){return a-b;});
+nums.sort((a,b)=>a-b);
+// sorting in random order
+// nums.sort(()=>0.5 - Math.random());
+// nums.sort(function(){return 0.5- Math.random();})
+
+//? console.log(nums[0]);
+//? console.log(nums[nums.length-1]);
+// Math.min  Math.max
+const minimum= minInArray(nums);
+function minInArray(arr){
+    return Math.min.apply(null,arr);
+}
+const maxInArray = (arr)=>Math.max.apply(null,arr);
+const maximum= maxInArray(nums);
+
+// Apply(null,[])
+function sum(a,b){
+    return a+b;
+}
+const result=sum.apply(null,[10,20]);
+
+// sorting Object Array
+cars=[
+    {
+        name:"Ford",
+        year:2002
+    },{
+        name:"audi",
+        year:2000
+    },{
+        name:"BMW",
+        year:2007
+    },{
+        name:"kia",
+        year:2010
+    }
+]
+cars.sort((a,b)=>a.year-b.year);
+// .sort() modifies the original array ✅ Using slice() to Copy Before Sorting:
+const ageSorted= employers.slice().sort((a,b)=>a.age-b.age);
+const gpaSorted= employers.slice().sort((a,b)=>b.gpa-a.gpa);
+console.log(ageSorted);
+console.log(gpaSorted);
